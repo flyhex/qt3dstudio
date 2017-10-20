7818cf858b1c55e1ac0416743f0dc880de604966
Tomi Korpipaa <tomi.korpipaa@qt.io>
2017-10-20 09:03:05 +0300

    Fix about dialog FBX copyright
    Task-number: QT3DS-131
    Change-Id: I22a95f178d8332f17cd942383859c229c992e18b
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- src/Authoring/Studio/_Win/Application/AboutDlg.cpp
+++ src/Authoring/Studio/_Win/Application/AboutDlg.cpp
@@ -33,6 +33,7 @@
 #include "StudioDefs.h"
 #include "Strings.h"
 #include "StringLoader.h"
+#include "qtAuthoring-config.h"
 
 //==============================================================================
 //	Includes
