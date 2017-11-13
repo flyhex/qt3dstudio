e29c3e50d2247330f8b89f80cb0ef16464d1d9a8
Miikka Heikkinen <miikka.heikkinen@qt.io>
2017-11-13 16:59:15 +0200

    Update open dialog
    Updated background image and added two recent files to make the list
    little less sparse.
    
    Change-Id: I448ebfa91ea495659886fff0bb19d1d8d7c0a07f
    Reviewed-by: Marianne Yrjänä <marianne.yrjana@qt.io>
    Reviewed-by: Antti Määttä <antti.maatta@qt.io>

--- src/Authoring/Studio/_Win/UI/StartupDlg.h
+++ src/Authoring/Studio/_Win/UI/StartupDlg.h
@@ -76,7 +76,7 @@ public:
     virtual ~CStartupDlg();
 
 protected:
-    const static int RECENT_COUNT = 5;
+    const static int RECENT_COUNT = 7;
 
     void paintEvent(QPaintEvent *event) override;
     void showEvent(QShowEvent *) override;
