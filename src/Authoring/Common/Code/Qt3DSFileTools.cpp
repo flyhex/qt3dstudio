87e24d340d6858f3e312ccbed14ad58d71224909
Määttä Antti <antti.maatta@qt.io>
2017-11-17 10:05:30 +0200

    Fix assert when opening presentation
    Do not return absolute path with CFilePath::GetDirectory
    
    Change-Id: I8712dd789ad457a81fd45bfd8b1a4f5caa96ad7a
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- src/Authoring/Common/Code/Qt3DSFileTools.cpp
+++ src/Authoring/Common/Code/Qt3DSFileTools.cpp
@@ -126,7 +126,7 @@ void CFilePath::normalizeAndSetPath(const QString &path)
 
 CFilePath CFilePath::GetDirectory() const
 {
-    return CFilePath(dir().canonicalPath());
+    return CFilePath(path());
 }
 
 CString CFilePath::GetFileStem() const
